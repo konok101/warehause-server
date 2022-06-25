d32b8a0771813ea75173c712270f34366e93bc1e
konok101 <rockykonok@gmail.com>
2022-06-25 22:51:24 +0600

    push for heroku

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
-const port= process.env.PORT || 7000;
+const port = process.env.PORT || 5000;
 
 
 //middleware
@@ -12,32 +12,38 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xlajaco.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
- 
-async function run(){
-  try{
+
+async function run() {
+  try {
     await client.connect();
     const fruitsCollection = client.db('FruitsHouse').collection('fruitsInfo');
-  app.get('/fruitsInfo', async(req,res)=>{
-    const query= {};
-    const cursor = fruitsCollection.find(query);
-    const fruits= await cursor.toArray();
-    res.send(fruits);
-  });
 
+    app.get('/fruitsInfo', async (req, res) => {
+      const query = {};
+      const cursor = fruitsCollection.find(query);
+      const fruits = await cursor.toArray();
+      res.send(fruits);
+    });
 
+    app.get('/fruitsInfo/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const fruit = await fruitsCollection.findOne(query);
+      res.send(fruit);
+    })
   }
-  finally{
+  finally {
 
   }
 }
-run().catch(console.dir);
+run().catch(console.log('rejected'));
 
 
 
-app.get('/', (req, res)=>{
-    res.send('Server is running');
+app.get('/', (req, res) => {
+  res.send('Server is running');
 });
 
-app.listen(port, ()=>{
-    console.log('server is running with port', port);
-})
+app.listen(port, () => {
+  console.log('server is running with port', port);
+});
