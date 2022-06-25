dbf3f104e6e4275f5eeb16c7622c60385497af89
konok101 <rockykonok@gmail.com>
2022-06-25 15:11:14 +0600

    Fruits info get done

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
-const port= process.env.PORT || 5000;
+const port= process.env.PORT || 7000;
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xlajaco.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+ 
+async function run(){
+  try{
+    await client.connect();
+    const fruitsCollection = client.db('FruitsHouse').collection('fruitsInfo');
+  app.get('/fruitsInfo', async(req,res)=>{
+    const query= {};
+    const cursor = fruitsCollection.find(query);
+    const fruits= await cursor.toArray();
+    res.send(fruits);
+  });
+
+
+  }
+  finally{
+
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res)=>{
     res.send('Server is running');
