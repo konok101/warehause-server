25e88594ad90b2cb5275672743edd23011b785e5
konok101 <rockykonok@gmail.com>
2022-06-28 13:08:21 +0600

    for heroku deploy

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
 
     app.get('/fruitsInfo/:id', async (req, res) => {
       const id = req.params.id;
+      console.log(id);
       const query = { _id: ObjectId(id) };
       const fruit = await fruitsCollection.findOne(query);
       res.send(fruit);
